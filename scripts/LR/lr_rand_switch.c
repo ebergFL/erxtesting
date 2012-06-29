69319dd117d0f832f3ba005325e3a857a359d713
erichberg <erichberg@gmail.com>
2012-06-29 19:50:53 +0000

    LoadRunner or ANSI C random switch snippet to replace the inelegant nested IF/ELSE

---
+++ scripts/LR/lr_rand_switch.c
@@ -0,0 +1,39 @@
+#define	SIZE	4
+
+main()
+{
+/*
+Code snippet Switch (% or ratio)
+use the switch/case statment to randomly execute "transactions" in loadrunner or functions in ANSI C
+*/
+
+	//declaration
+	const int list[SIZE] = {15,15,25,45};	//Percentage each action should be executed
+	int i, j, k, l;
+	
+	srand(time(NULL));
+	i = rand() % 100 + 1;  //Generate a number between 1 and 100 (percentage)
+	for (j=k=l=0; j<SIZE; k+=i>(l+=list[j++]));	//Set k equal to a value from 0 to (SIZE - 1)
+	lr_output_message("SIZE=%i i=%i j=%i k=%i l=%i",SIZE,i,j,k,l);
+	
+	switch(k) {
+	//  will execute Actions (functions) using numeric percentage values, in this case, but can be used with other ratios
+		case 0:		Action1();    //15%
+		   break;
+		case 1:		Action2();    //15%
+		   break;
+		case 2:		Action3();    //25%
+		   break;
+		case 3:		Action4();    //45%
+		   break;
+		default:	errorz();
+		}
+	
+	return 0;
+}
+
+Action1(){lr_message("This is the 1st action");}
+Action2(){lr_message("This is the 2nd action");}
+Action3(){lr_message("This is the 3rd action");}
+Action4(){lr_message("This is the 4th action");}
+errorz(){lr_message("~~~Shouldn't see this unless you missed a value");}
