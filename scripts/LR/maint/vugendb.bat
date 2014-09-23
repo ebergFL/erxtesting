7e8dec10e03a4dab1d01635a89f8d9054169c102
erichberg <erichberg@gmail.com>
2014-09-23 20:47:37 +0000

    v09.12

--- scripts/LR/maint/vugendb.bat
+++ scripts/LR/maint/vugendb.bat
@@ -1,22 +1,21 @@
-::==v09.11
+::==v09.12
 @echo off
 cls
+::Set VUGEN_PATH_REMOTE, scriptList, dataGroup, and vugenSvr
+call %~n0.init.bat
 
-rem defaults
-set file=live
-set env=ash
-set svr=ashprdldr01
-
-dir /d *.
-set /p action=Using input list file "%file%" (Y/Name)? 
-if /i "%action%"=="Y" (goto start) else set file=%action%
+echo Files in working directory:
+dir /d /b *.
+echo Using input list file "%scriptList%"
+set /p action=press enter to accept or type name of file: 
+if "%action%"=="" (goto start) else set scriptList=%action%
 
 :start
 cls
 echo =======================================
-echo Using file "%file%", would you like to:
-echo C change the dat files (default "%env%")
-echo S change the remote server (default "%svr%")
+echo Using file "%scriptList%", would you like to:
+echo C change the dat files (current "%dataGroup%")
+echo S change the remote server (current "%vugenSvr%")
 echo R remotely run the scripts (PsExec.exe required)
 echo L locally run the scripts
 echo V view the output files
@@ -24,26 +23,25 @@ echo O open the result1 report
 echo E edit the script files
 echo X exit
 echo =======================================
+set /p action=Please enter a letter C, S, R, L, V, O, E or X: 
 
-set /p action=Please enter a letter C, R, L, V, O, E or X: 
-
-if /i "%action%"=="X" goto exit 
-if /i "%action%"=="C" set /p env=Please enter environment to work in "ash", "sec", sc3 or "int"...: 
-if /i "%action%"=="S" set /p svr=Please enter remote server to run in "ashprdldr01", "secprdldr01", or "local"...: 
+if /i "%action%"=="X" exit /b
+if /i "%action%"=="R" if /i "%ERX_PASS%"=="" set /p ERX_PASS=Enter your password for remote %USERDOMAIN%\%USERNAME%: & cls
+if /i "%action%"=="C" set /p dataGroup=Please enter dat file group to work in "dev", "uat", or "int" (current "%dataGroup%")...: 
+if /i "%action%"=="S" set /p vugenSvr=Please enter remote server to run in "psin0p280", etc. (current "%vugenSvr%")..: 
 
 setLocal EnableDelayedExpansion
-for /f "tokens=1,2 delims= " %%a in (%file%) do (
-if /i "%action%"=="C" copy %%a\%%b\*.dat.%env% %%a\%%b\*.
-if /i "%action%"=="R" psexec \\%svr% -u gsiccorp\berge -p %ERX_PWD2% -e -w C:\Progra~1\HP\LoadRunner\bin -d C:\Progra~1\HP\LoadRunner\bin\mmdrv.exe -usr -usr %%a\%%b\%%b.usr
-if /i "%action%"=="L" start "execute script %%b" cmd /c "%VUGEN_PATH%bin\mmdrv.exe" -usr %%a\%%b\%%b.usr
-if /i "%action%"=="V" start "view %%b output" cmd /c %%a\%%b\output.txt
-if /i "%action%"=="O" pause & start "view %%b result1" cmd /c "%VUGEN_PATH%bin\QTReport.exe" %%a\%%b\result1\Results.qtp
-if /i "%action%"=="E" pause & start "edit %%b script" %%a\%%b\%%b.usr
+for /f "tokens=1,2" %%a in (%scriptList%) do (
+	if /i "%action%"=="C" copy %%a\%%b\*.dat.%dataGroup% %%a\%%b\*.
+	::Use System account -s, don't load profile -e, set working directory -w, don't wait for process to terminate -d
+	if /i "%action%"=="R" psexec \\%vugenSvr% -u %USERDOMAIN%\%USERNAME% -p %ERX_PASS% -e -w "%VUGEN_PATH_REMOTE%" -d "%VUGEN_PATH_REMOTE%\bin\mmdrv.exe" -usr %%a\%%b\%%b.usr
+	if /i "%action%"=="L" start "execute script %%b" cmd /c "%VUGEN_PATH%bin\mmdrv.exe" -usr %%a\%%b\%%b.usr
+	if /i "%action%"=="V" start "view %%b output" cmd /c %%a\%%b\output.txt
+	if /i "%action%"=="O" pause & start "view %%b result1" cmd /c "%VUGEN_PATH%bin\QTReport.exe" %%a\%%b\result1\Results.qtp
+	if /i "%action%"=="E" pause & start "edit %%b script" %%a\%%b\%%b.usr
 )
-pause
 
-goto start
-:exit
+pause & goto start
 
 ::Notes:
 ::Add to batch file to prep Live scripts:
@@ -55,8 +53,6 @@ goto start
 :: then search for .SEC and .ASH lines
 :: Vuser Terminated.	[MsgId: MMSG-15963]
 
-::Sample format for live.txt file of path and script, note the space before the file script name-
-::E:\PE\Script\LR\Live PageNavigation_new_ec2
-::E:\PE\Script\LR\Live Search_Script_new_EC2
-
-::==
+::Sample format for live.txt file of path and script, note the tab delimiter before the file script name-
+::E:\PE\Script\LR\Live	PageNavigation
+::E:\PE\Script\LR\Live	Search_Script
